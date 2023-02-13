1a0d52205a0a88e3104faf7bcaaa0027bef92388
Jerry <chiemenagodson532@gmail.com>
2023-02-13 19:54:41 +0100

    First Commit

---
+++ jerry/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from .models import *
+
+def index(request):
+    home = Home.objects.latest('updated')
+
+    about = About.objects.latest('updated')
+    profiles = Profile.objects.filter(about=about)
+
+    categories = Category.objects.all()
+
+    portfolios = Portfolio.objects.all()
+
+    context = {
+        'home':home,
+        'about':about,
+        'profiles':profiles,
+        'categories': categories,
+        'portfolios': portfolios,
+    }
+
+
+    return render(request, 'index.html', context)
+
